3403fb644fe335b6a91efb6a6c03a4224eb2451b
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-03-12 19:11:53 -0300

    cumpre requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -7,7 +10,10 @@ class Search extends React.Component {
 
     this.state = {
       theSearch: '',
+      saveSearch: '',
       isButtonDisabled: true,
+      loading: false,
+      albums: [],
     };
   }
 
@@ -35,8 +41,23 @@ class Search extends React.Component {
     }
   }
 
+  handleClick = async () => {
+    const { theSearch } = this.state;
+    this.setState({
+      loading: true,
+      theSearch: '',
+      saveSearch: theSearch,
+    });
+    const album = await searchAlbumsAPI(theSearch);
+    this.setState({
+      loading: false,
+      albums: album,
+    });
+  }
+
   render() {
-    const { theSearch, isButtonDisabled } = this.state;
+    const { theSearch, isButtonDisabled, loading, albums, saveSearch } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
@@ -53,11 +74,36 @@ class Search extends React.Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ isButtonDisabled }
+            onClick={ this.handleClick }
           >
             Pesquisar
           </button>
         </form>
-        search.
+        {loading && <Loading />}
+        {!loading && albums.length === 0
+        && <h2>Nenhum álbum foi encontrado</h2>}
+        {!loading && albums.length !== 0 && (
+          <>
+            <h2>{`Resultado de álbuns de: ${saveSearch}`}</h2>
+            <ul>
+              {albums.map((album) => (
+                <li key={ album.collectionId }>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    <img
+                      src={ album.artworkUrl100 }
+                      alt={ `${album.collectionName} - ${album.artistName}` }
+                    />
+                    <h3>{ album.collectionName }</h3>
+                    <p>{ album.artistName }</p>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
       </div>
     );
   }
